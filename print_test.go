57e7160c250d3eccda47c0cad94c449b8654e92d
Benjamin Borbe <bborbe@rocketnews.de>
2019-03-26 11:14:59 +0100

    remove glog

--- print_test.go
+++ print_test.go
@@ -5,21 +5,38 @@
 package argument_test
 
 import (
+	"bytes"
 	"github.com/bborbe/argument"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"log"
 )
 
 var _ = Describe("Print", func() {
-	It("print without error", func() {
-		args := struct {
-			Username string
-			Password string `display:"length"`
-			Debug    bool   `display:"hidden"`
-		}{
+	type app struct {
+		Username string
+		Password string `display:"length"`
+		Debug    bool   `display:"hidden"`
+	}
+	var buf *bytes.Buffer
+	var args app
+	BeforeEach(func() {
+		buf = &bytes.Buffer{}
+		log.SetOutput(buf)
+		log.SetFlags(0)
+		args = app{
 			Username: "Ben",
+			Password: "S3CR3T",
+			Debug:    true,
 		}
+	})
+	It("print without error", func() {
 		err := argument.Print(&args)
 		Expect(err).NotTo(HaveOccurred())
+	})
+	It("foo", func() {
+		err := argument.Print(&args)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(buf.String()).To(Equal("Argument: Username 'Ben'\nArgument: Password length 6\n"))
 	})
 })

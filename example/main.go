d42a1d0b6cd73c2f97e53cd796adab0502850c75
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-10-15 11:25:15 +0200

    extend examples

--- example/main.go
+++ example/main.go
@@ -6,10 +6,13 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
+	"os"
+	"time"
 
 	"github.com/bborbe/argument/v2"
+	libtime "github.com/bborbe/time"
 )
 
 type Username string
@@ -21,13 +24,28 @@ type Active bool
 func main() {
 	ctx := context.Background()
 	var data struct {
-		Username Username `arg:"username" default:"ben"`
-		Password Password `arg:"password" display:"length"`
-		Active   *Active  `arg:"active"`
-		Url      string   `arg:"url"`
+		Username          Username         `arg:"username" default:"ben"`
+		Password          Password         `arg:"password" display:"length"`
+		Active            *Active          `arg:"active"`
+		Url               string           `arg:"url"`
+		DefaultWithoutArg string           `arg:"defaultWithoutArg" default:"hello world"`
+		DefaultWithArg    string           `arg:"defaultWithArg" default:"hello world"`
+		Int               int              `arg:"int"`
+		Float64           float64          `arg:"float64"`
+		Float64Ptr        *float64         `arg:"float64Ptr"`
+		StdDuration       time.Duration    `arg:"std-duration"`
+		StdTime           time.Time        `arg:"std-time"`
+		Duration          libtime.Duration `arg:"duration"`
+		DateTime          libtime.DateTime `arg:"datetime"`
+		Date              libtime.Date     `arg:"date"`
+		UnixTime          libtime.UnixTime `arg:"unixtime"`
 	}
 	if err := argument.Parse(ctx, &data); err != nil {
 		log.Fatalf("parse args failed: %v", err)
 	}
-	fmt.Printf("%+v\n", data)
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(data); err != nil {
+		log.Fatalf("encode data failed: %v", err)
+	}
 }
